5f8899bfead7c068267b986e74997eb8b71662e4
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2022-10-31 13:47:54 +0530

    Add files via upload

---
+++ question2/Max.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+
+using namespace std;
+
+int main(){ 
+   cout<<"Memory size of int="<<sizeof(int)<<"bytes"<<endl;
+   cout<<"Memory size of float="<<sizeof(int)<<"bytes"<<endl;
+   cout<<"Memory size of bool="<<sizeof(bool)<<"bytes"<<endl;
+   cout<<"Memory size of char="<<sizeof(bool)<<"bytes"<<endl;
+
+   return 0;
+
+}
+
