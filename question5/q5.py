4795cec0664d6ac492fa725da63b21d7e678d68e
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2022-11-03 19:50:52 +0530

    Update q5.py

--- question5/q5.py
+++ question5/q5.py
@@ -1,5 +1,5 @@
 from Fibonacci import Fibo
 
-print("The 10th, 20th and 30th Fibonacci numbers are respectively", Fibo(10)[0], F(ibo20)[0], Fibo(30)[0])
+print("The 10th, 20th and 30th Fibonacci numbers are respectively", Fibo(10)[0], Fibo(20)[0], Fibo(30)[0])
 
 
